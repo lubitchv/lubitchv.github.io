8816f330816a66ff4d6ef775be0f1cd976592b22
Victoria Lubitch <43550154+lubitchv@users.noreply.github.com>
2021-03-16 15:14:43 -0400

    test

--- README.md
+++
@@ -1,5 +0,0 @@
-# globus
-
-_In development_.
-
-A Dataverse external tool for globus integration to enable larger file uploads.
--- src/app/globus.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GlobusDirective } from './globus.directive';
-
-describe('GlobusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new GlobusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
